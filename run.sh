fd36e688fa23fbdab8ae04d27c13be2563233b7a
Chad Homan <choman@gmail.com>
2016-10-27 15:25:14 -0500

    Quick capture - files in flux

--- run.sh
+++ run.sh
@@ -17,15 +17,16 @@ app=crdp
 
 uid=0
 
+        ##--network isolated_nw \
+        ##-v /dev/shm:/dev/shm \
+        ##-v /etc/machine-id:/etc/machine-id \
+        ##-v /run/user/$uid/pulse:/run/user/$uid/pulse \
+        ##-v /var/lib/dbus:/var/lib/dbus \
+        ##-v ~/.pulse:/.pulse \
+        ##-v /dev/snd:/dev/snd \
 
 docker run -it --rm -e DISPLAY=$DISPLAY \
         -v /tmp/.X11-unix:/tmp/.X11-unix \
-        -v /dev/shm:/dev/shm \
-        -v /etc/machine-id:/etc/machine-id \
-        -v /run/user/$uid/pulse:/run/user/$uid/pulse \
-        -v /var/lib/dbus:/var/lib/dbus \
-        -v ~/.pulse:/.pulse \
-        -v /dev/snd:/dev/snd \
         $app "$@"
 
 xhost - local:docker
